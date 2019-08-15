76e98070cb08fcd988ea5d31154cd82bc1214b36
Renan Gonçalves <renan.saddam@gmail.com>
2019-08-15 14:24:29 +0200

    Expose metrics for Prometheus

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/MarijnKoesen/replikator-api
 go 1.12
 
 require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
 	github.com/droundy/goopt v0.0.0-20170604162106-0b8effe182da
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/gorilla/mux v1.7.3
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.1.0
 )
---
+++ metrics.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+var (
+	// General metrics
+	httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Count of all HTTP requests",
+	}, []string{"code", "method"})
+
+	// Replikator/Replication metrics
+	replikatorReplicationLag = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "replikator_replication_lag",
+		Help: "Replication lag from master server",
+	}, []string{"state"})
+
+	replikatorDiskCapacity = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "replikator_disk_capacity",
+		Help: "Disk capacity",
+	})
+
+	replikatorMemoryCapacity = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "replikator_memory_capacity",
+		Help: "Memory capacity",
+	})
+
+	// Replicas metrics
+	replikatorReplicaDiskUsage = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "replikator_replica_disk_usage",
+		Help: "Disk usage by a replica",
+	}, []string{"replica", "state"})
+
+	replikatorReplicaMemoryAllocated = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "replikator_replica_memory_allocated",
+		Help: "Memory allocated for a replica",
+	}, []string{"replica", "state"})
+
+	replikatorReplicaMemoryUsed = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "replikator_replica_memory_used",
+		Help: "Memory used by a replica",
+	}, []string{"replica", "state"})
+)
+
+type replikatorData struct {
+	DatabaseGlobalState databaseGlobalState `json:"DatabaseGlobalState"`
+}
+
+type databaseGlobalState struct {
+	DatabaseInstanceState []databaseInstanceState `json:"DatabaseInstanceState"`
+	ReplicationState      string                  `json:"eReplicationState"`
+	ReplicationLag        string                  `json:"iReplicationLag"`
+	DiskCapacity          string                  `json:"sTotalStorageCapacity"`
+	MemoryCapacity        string                  `json:"sTotalMemCapacity"`
+}
+
+type databaseInstanceState struct {
+	DatabaseProperties databaseProperties `json:"DatabaseProperties"`
+	State              string             `json:"eState"`
+	DiskUsage          string             `json:"sSizeTotal"`
+	MemoryAllocated    string             `json:"sMemAllocated"`
+	MemoryUsed         string             `json:"sMemUsed"`
+}
+
+type databaseProperties struct {
+	InstanceId string `json:"sInstanceId"`
+}
+
+func registerMetrics() {
+	prometheus.MustRegister(
+		// General metrics
+		httpRequestsTotal,
+
+		// Replikator/Replication metrics
+		replikatorReplicationLag,
+		replikatorDiskCapacity,
+		replikatorMemoryCapacity,
+
+		// Replicas metrics
+		replikatorReplicaDiskUsage,
+		replikatorReplicaMemoryAllocated,
+		replikatorReplicaMemoryUsed,
+	)
+}
+
+func getMetrics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var data replikatorData
+
+		output := execute("--output json --list")
+		json.Unmarshal([]byte(output), &data)
+
+		replicationLag, err := strconv.ParseFloat(data.DatabaseGlobalState.ReplicationLag, 64)
+		if err != nil {
+			replicationLag = -1
+		}
+		replikatorReplicationLag.With(prometheus.Labels{"state": strings.ToLower(data.DatabaseGlobalState.ReplicationState)}).Set(replicationLag)
+
+		diskCapacity, _ := strconv.ParseFloat(data.DatabaseGlobalState.DiskCapacity, 64)
+		replikatorDiskCapacity.Set(diskCapacity)
+
+		memoryCapacity, _ := strconv.ParseFloat(data.DatabaseGlobalState.MemoryCapacity, 64)
+		replikatorMemoryCapacity.Set(memoryCapacity)
+
+		replikatorReplicaDiskUsage.Reset()
+		replikatorReplicaMemoryAllocated.Reset()
+		replikatorReplicaMemoryUsed.Reset()
+
+		for _, replikator := range data.DatabaseGlobalState.DatabaseInstanceState {
+			labels := prometheus.Labels{
+				"replica": replikator.DatabaseProperties.InstanceId,
+				"state":   strings.ToLower(replikator.State),
+			}
+
+			diskUsage, err := strconv.ParseFloat(replikator.DiskUsage, 64)
+			if err != nil {
+				diskUsage = 0
+			}
+
+			memoryAllocated, err := strconv.ParseFloat(replikator.MemoryAllocated, 64)
+			if err != nil {
+				memoryAllocated = 0
+			}
+
+			memoryUsed, err := strconv.ParseFloat(replikator.MemoryUsed, 64)
+			if err != nil {
+				memoryUsed = 0
+			}
+
+			replikatorReplicaDiskUsage.With(labels).Set(diskUsage)
+			replikatorReplicaMemoryAllocated.With(labels).Set(memoryAllocated)
+			replikatorReplicaMemoryUsed.With(labels).Set(memoryUsed)
+		}
+
+		promhttp.Handler().ServeHTTP(w, r)
+	})
+}
